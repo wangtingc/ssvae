d667cf4eba5bb7b15c89cef610e51561ae8ca1e3
Wead Hsu <wead_hsu@163.com>
2016-01-11 15:50:32 +0800

    modify preprocess to filt <br />

--- scripts/preprocess/imdb_preprocess.py
+++ scripts/preprocess/imdb_preprocess.py
@@ -38,23 +38,27 @@ def tokenize(sentences):
     return toks
 
 
+def filt_line(line):
+    return line.strip().replace('<br />', ' ')
+
+
 def build_dict(path, include_unlabel):
     sentences = []
     currdir = os.getcwd()
     os.chdir('%s/pos/' % path)
     for ff in glob.glob("*.txt"):
         with open(ff, 'r') as f:
-            sentences.append(f.readline().strip())
+            sentences.append(filt_line(f.readline()))
     os.chdir('%s/neg/' % path)
     for ff in glob.glob("*.txt"):
         with open(ff, 'r') as f:
-            sentences.append(f.readline().strip())
+            sentences.append(filt_line(f.readline()))
 
     if include_unlabel:
         os.chdir('%s/unsup/' % path)
         for ff in glob.glob("*.txt"):
             with open(ff, 'r') as f:
-                sentences.append(f.readline().strip())
+                sentences.append(filt_line(f.readline()))
 
     os.chdir(currdir)
 
@@ -91,7 +95,7 @@ def grab_data(path, dictionary):
     os.chdir(path)
     for ff in glob.glob("*.txt"):
         with open(ff, 'r') as f:
-            sentences.append(f.readline().strip())
+            sentences.append(filt_line(f.readline()))
     os.chdir(currdir)
     sentences = tokenize(sentences)
 
@@ -104,16 +108,24 @@ def grab_data(path, dictionary):
 
 
 def main():
-    include_unlabel = True
+    data_type = 'u_sd' #('l', 'u', 'u_sd')
+    print('Building data with', data_type)
+
     # Get the dataset from http://ai.stanford.edu/~amaas/data/sentiment/
     path = '/home/wead/datasets/aclImdb/'
-    dictionary = build_dict(os.path.join(path, 'train'), include_unlabel)
+    if data_type == 'l' or data_type == 'u_sd':
+        dictionary = build_dict(os.path.join(path, 'train'), False)
+    else:
+        dictionary = build_dict(os.path.join(path, 'train'), True)
 
     train_x_pos = grab_data(path+'train/pos', dictionary)
     train_x_neg = grab_data(path+'train/neg', dictionary)
-    train_x_unl = grab_data(path+'train/unsup', dictionary) if include_unlabel else []
+    if data_type == 'u' or data_type == 'u_sd':
+        unlabel_x = grab_data(path+'train/unsup', dictionary)
+    else:
+        unlabel_x = []
 
-    train_x = train_x_pos + train_x_neg + train_x_unl
+    train_x = train_x_pos + train_x_neg
     train_y = [1] * len(train_x_pos) + [0] * len(train_x_neg)
 
     test_x_pos = grab_data(path+'test/pos', dictionary)
@@ -121,18 +133,15 @@ def main():
     test_x = test_x_pos + test_x_neg
     test_y = [1] * len(test_x_pos) + [0] * len(test_x_neg)
 
-    if include_unlabel:
-        f = gzip.open('../../data/proc/imdb_u.pkl.gz', 'wb')
-    else:
-        f = gzip.open('../../data/proc/imdb_l.pkl.gz', 'wb')
+    f_name = '../../data/proc/imdb_%s.pkl.gz' % data_type
+    f = gzip.open(f_name, 'wb')
     pkl.dump((train_x, train_y), f, -1)
     pkl.dump((test_x, test_y), f, -1)
+    pkl.dump(unlabel_x, f, -1)
     f.close()
 
-    if include_unlabel:
-        f = gzip.open('../../data/proc/imdb_u.dict.pkl.gz', 'wb')
-    else:
-        f = gzip.open('../../data/proc/imdb_l.dict.pkl.gz', 'wb')
+    f_name = '../../data/proc/imdb_%s.dict.pkl.gz' % data_type
+    f = gzip.open(f_name, 'wb')
     pkl.dump(dictionary, f, -1)
     f.close()
 
