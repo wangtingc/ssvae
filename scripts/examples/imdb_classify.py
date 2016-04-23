1) change lr to 0.001 imdb_train.py for fast training 2) fix bugs in imdb_classify and modify rnnclf.py by an additional 30 units layer 3) fix bugs in sc_lstm_2.py, i.e., forget to add W_da_to_forgetgate param

scripts/examples/imdb_classify.py
@@ -30,22 +30,22 @@ def init_configurations():
     params['data_path'] = '../../data/proc/imdb/imdb_u.pkl.gz' # to be tested
     params['dict_path'] = '../../data/proc/imdb/imdb_u.dict.pkl.gz'
     params['emb_path'] = '../../data/proc/imdb/imdb_emb_u.pkl.gz'
-    params['batch_size'] = 100
+    params['batch_size'] = 25
     params['num_classes'] = 2
     params['dim_emb'] = 300
     params['num_units'] = 512
     params['num_samples_train'] = 20000 # the first n samples in trainset.
     params['epoch'] = 200
-    params['dev_period'] = 5 # temporary exclude validset
-    params['test_period'] = 5
-    params['lr'] = 0.0002
+    params['dev_period'] = 1 # temporary exclude validset
+    params['test_period'] = 2
+    params['lr'] = 0.0004
     params['num_words'] = 20000
-    params['dropout'] = 0.5 # set 0 to no use
+    params['dropout'] = 0.80 # set 0 to no use
     params['exp_time'] = datetime.now().strftime('%m%d%H%M')
     params['save_dir'] = '../../results/' + params['exp_name'] + '_' + params['exp_time']
     params['save_weights_path'] = params['save_dir'] + '/weights.pkl'
 
-    params['pretrain_load_path'] = '../../data/proc/imdb/pretrain_lm.pkl'
+    params['pretrain_load_path'] = '../../data/proc/imdb/pretrain_lm2.pkl'
     params['use_final'] = True
     return params
 
@@ -133,7 +133,7 @@ def train(params):
                 x, m = prepare_data(x)
                 dev_acc = f_test(x, m, y)
                 dev_accs.append(dev_acc)
-                df.write('\t[-] valid: batch %d, acc %2f\n' % (batch, dev_cost, dev_acc))
+                df.write('\t[-] valid: batch %d, acc %2f\n' % (batch, dev_acc))
 
         dev_epoch_accs.append(np.mean(dev_accs))
         print('dev_accuracy.mean()', np.mean(dev_accs))